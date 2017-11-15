dd92a7a5dc81cf14f1ab77da209d593c40bc56da
Remmy Clay <remmy.lauren@gmail.com>
2017-11-15 10:33:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,19 +2,3 @@ def reverse_each_word(sentence)
   reversed_sentence = sentence.split().collect {|word| word.reverse}
   reversed_sentence.join(" ")
 end
-
-
-
-
-
-
-
-
-#def reverse_each_word(sentence)
-#    reverse_array = []
-#    array = sentence.split()
-#    array.each do |word|
-#      reverse_array << word.reverse
-#    end
-#    return reverse_array.join(" ")
-#end
