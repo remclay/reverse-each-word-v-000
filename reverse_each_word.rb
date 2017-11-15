5033d993d2038920e1e34f54b19c6a12e9740a84
Remmy Clay <remmy.lauren@gmail.com>
2017-11-15 10:33:25 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split()
-  reversed_sentence = sentence_array.collect {|word| word.reverse}
+  reversed_sentence = sentence.split().collect {|word| word.reverse}
   reversed_sentence.join(" ")
 end
 
